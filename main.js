e967a999d3bf0c24bfcbbe256a357422f8b08d1b
Derek Farr <dfarr3@gmail.com>
2025-08-01 22:10:11 -0400

    clean up and refactor code

--- main.js
+++ main.js
@@ -8,12 +8,14 @@ const modalOuterElement = document.querySelector('.modal-outer');
 const modalInnerElement = document.querySelector('.modal-inner');
 const modalHeader = modalInnerElement.querySelector('h2');
 
+
 let recipes = [];
-let uploadedImage;
+
 
 /* TODOs:
     - Allow users to upload images for recipes. Store images (perhaps all recipe data) in IndexedDb
     - Add search to filter list of recipes
+    - Add support for images
     - New flow for editing recipes: Click '...' button in corner of card to see options
     - Only show card w/ recipe name, ingredients, and image on main screen. click 'see more' to open modal to see full recipe.
     - Format recipe text: Capitalize first letter of each field, undoubtedly more...
@@ -21,23 +23,26 @@ let uploadedImage;
 */
 
 
-function mirrorRecipesToLocalStorage() {
-    localStorage.setItem('recipes', JSON.stringify(recipes));
-}
 
+/* ---------- MOVE DATA TO/FROM STORAGE ---------- */
 
-function restoreFromLocalStorage() {
-    console.log('Restoring recipes from local storage...');
+function restoreRecipesFromLocalStorage() {
     const storedRecipes = JSON.parse(localStorage.getItem('recipes'));
 
     if (storedRecipes && storedRecipes.length) {
         recipes.push(...storedRecipes);
         recipeListElement.dispatchEvent(new CustomEvent('recipesUpdated'));
-    } else {
-        console.log('No recipes in local storage')
     }
 }
 
+
+function mirrorRecipesToLocalStorage() {
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+}
+
+
+
+/* ---------- MANIPULATE DATA IN MEMORY ---------- */
 
 function addRecipe(recipe) {
     recipes.push(recipe);
@@ -57,152 +62,65 @@ function deleteRecipe(id) {
     recipeListElement.dispatchEvent(new CustomEvent('recipesUpdated'));
 }
 
-function displayFullRecipe(recipe) {
-    const html = 
-        `<div class="recipe" data-id="${recipe.id}">
-            <h2 class="recipe-name">${recipe.name}</h2>
-            <p>${recipe.description}</p>
-            <h3>Ingredients</h3>
-            <ul class="recipe-ingredients">${recipe.ingredients.map((ingredient) => 
-                `<li>${ingredient}</li>`).join('')}</ul>
-            <h3>Instructions</h3>
-            <ol class="recipe-instructions">${
-                recipe.instructions.map((step) => `<li>${step}</li>`).join('')
-            }</ol>
-            <button class="edit-recipe">Edit</button>
-            <button class="delete-recipe">Delete</button>
-        </div>`
-    // display in modal??
-}
 
 
-function displayRecipeCards() {
-    const html = recipes.map((recipe) => 
-        `<div class="recipe-card" data-id="${recipe.id}">
-            <div class="recipe-card-img-container" >
-                <img src="${recipe.photo || './assets/icons/fast-food-100.png'}" ${recipe.photo ? '' : 'class="placeholder"'} alt="${recipe.name}" />
-            </div>
-            <div class="recipe-card-body">
-                <div class="recipe-card-header">
-                    <h2 class="recipe-name">${recipe.name}</h2>
-                    <div class="options-dropdown">
-                        <input type="image" src="./assets/icons/3-dots_black.png" class="see-options" aria-label="See more options" name="options" />
-                        <div class="options">
-                            <a class="edit-recipe">Edit</a>
-                            <a class="delete-recipe">Delete</a>
+/* ---------- GET DATA IN MEMORY ---------- */
+
+const getRecipeById = (recipeId) => recipes.find((recipe) => recipe.id === recipeId);
+
+
+
+/* ---------- DEFINE UI COMPONENTS ---------- */
+
+const getTextInputHtml = ({classList, name, value}) => 
+    `<input type="text" class="${classList}" name="${name}" value="${value}" />`;
+
+
+const getTextInputSetHtml = (count, {classList, nameStart}, values) => 
+    Array.from({length: count})
+        .map((_, index) => 
+            getTextInputHtml({
+                classList,
+                name: nameStart + index,
+                value: values[index] || "",
+            }
+        ))
+        .join('');
+
+
+function getRecipeCardHtml(recipe) {
+    return `<div class="recipe-card" data-id="${recipe.id}">
+                <div class="recipe-card-img-container" >
+                    <img src="${recipe.photo || './assets/icons/fast-food-100.png'}" ${recipe.photo ? '' : 'class="placeholder"'} alt="${recipe.name}" />
+                </div>
+                <div class="recipe-card-body">
+                    <div class="recipe-card-header">
+                        <h2 class="recipe-name">${recipe.name}</h2>
+                        <div class="options-dropdown">
+                            <input type="image" src="./assets/icons/3-dots_black.png" class="see-options" aria-label="See more options" name="options" />
+                            <div class="options">
+                                <a class="edit-recipe">Edit</a>
+                                <a class="delete-recipe">Delete</a>
+                            </div>
                         </div>
                     </div>
+                    <ul class="recipe-ingredients">${recipe.ingredients.map((ingredient) => 
+                        `<li>${ingredient}</li>`).join('')}
+                    </ul>
                 </div>
-                <ul class="recipe-ingredients">${recipe.ingredients.map((ingredient) => 
-                    `<li>${ingredient}</li>`).join('')}
-                </ul>
-            </div>
-        </div>`
-    ).join('');
-
-    recipeListElement.innerHTML = html;
-}
-
-function addFieldsToFieldset(fieldset, values=[null]) {
-    const columnCount = getComputedStyle(fieldset).gridTemplateColumns.split(' ').length;
-    const newFieldCount = Math.ceil(values.length / columnCount) * columnCount;
-
-    const inputs = fieldset.querySelectorAll('input');
-    const lastInput = inputs[inputs.length - 1];
-
-    const classList = fieldset.dataset.childClassList;
-    const firstInputName = lastInput ? (Number(lastInput.name) + 1) : 0;
-
-    const newFieldHtml = Array.from({length: newFieldCount})
-        .map((_, index) => 
-            `<input type="text" class="${classList}" name="${firstInputName + index}" value="${values[index] || ""}" />`
-        ).join('');
-
-    if (lastInput) {
-        lastInput.insertAdjacentHTML('afterend', newFieldHtml)
-    } else {
-        fieldset.insertAdjacentHTML('afterbegin', newFieldHtml)
-    }
-}
-
-function inputsInFieldset(fieldset) {
-    return [...fieldset.children]
-        .filter((element) => element.matches('input'));
+            </div>`
 }
 
 
-function valuesInFieldset(fieldset) {
-    return inputsInFieldset(fieldset)
-        .filter((input) => input.value)
-        .map((input) => input.value);
+function getRecipeListHtml() {
+    return recipes.map(getRecipeCardHtml).join('');
 }
 
 
-function handleSave(event) {
-    event.preventDefault();
-    
-    const recipe = {
-        id: event.currentTarget.dataset.id || crypto.randomUUID(),
-        name: event.currentTarget.name.value, 
-        description: event.currentTarget.description.value,
-        ingredients: valuesInFieldset(ingredientsFieldset),
-        instructions: valuesInFieldset(instructionsFieldset),
-    };
-    return recipe;
-}
 
+/* ---------- UPDATE THE DOM BASED ON DATA ---------- */
 
-function handleAdd(event) {
-    const recipe = handleSave(event);
-    console.log(event)
-    addRecipe(recipe);
-}
-
-
-function handleUpdate(event) {
-    const recipe = handleSave(event);
-    event.currentTarget.removeAttribute('data-id');
-    updateRecipe(recipe);
-}
-
-
-function openModal() {
-    modalOuterElement.classList.add('open');
-}
-
-
-function closeModal() {
-    modalOuterElement.classList.remove('open');
-    recipeForm.removeAttribute('data-id');
-
-    [ingredientsFieldset, instructionsFieldset]
-        .forEach((fieldset) => 
-            [...fieldset.querySelectorAll('input')]
-                .forEach((input) => 
-                    input.remove()) );
-    
-    recipeForm.reset();
-}
-
-function populateAddForm() {
-    modalHeader.textContent = 'New recipe';
-    addFieldsToFieldset(ingredientsFieldset, Array.from({length: 8}));
-    addFieldsToFieldset(instructionsFieldset, Array.from({length: 4}));
-}
-
-function populateEditForm(recipeId) {
-
-    modalHeader.textContent = 'Edit recipe';
-    
-    const recipe = recipes.find((recipe) => recipe.id === recipeId);
-        
-    recipeForm.setAttribute('data-id', recipeId);
-    recipeForm.name.value = recipe.name;
-    recipeForm.description.value = recipe.description;
-
-    addFieldsToFieldset(ingredientsFieldset, recipe.ingredients);
-    addFieldsToFieldset(instructionsFieldset, recipe.instructions);
-}
+/* Update state of recipe options menu */
 
 function toggleOptionsMenu(event) {
     const seeOptionsButton = event.target.closest('.recipe-card').querySelector('.see-options');
@@ -212,111 +130,232 @@ function toggleOptionsMenu(event) {
 }
 
 
-function handleRecipeClick(event) {
+/* Update state of recipe list */
 
-    if (event.target.matches('.see-options')) {
-        toggleOptionsMenu(event);
-    }
-
-    const recipeId = event.target.closest('.recipe-card')?.dataset.id;
-    
-    if (event.target.matches('.edit-recipe')) {
-        toggleOptionsMenu(event);
-        populateEditForm(recipeId);
-        openModal();
-    }
-    
-    if (event.target.matches('.delete-recipe')) {
-        toggleOptionsMenu(event);
-        deleteRecipe(recipeId);
-    }
-    
+function displayRecipeCards() {
+    const html = getRecipeListHtml();
+    recipeListElement.innerHTML = html;
 }
 
 
-function handleModalClick(event) {
-    const isInInnerModal = event.target.closest('.modal-inner');
-    const parentFieldset = event.target.closest('fieldset');
+/* Update state of recipe form */
 
-    if (
-        !isInInnerModal 
-        || event.target.matches('button[name="cancel"]')
-    ) {
-        closeModal();
-    } else if (parentFieldset) {
-        addFieldsToFieldset(parentFieldset);
-    }
+const clearFieldsFromFieldset = (fieldset) => 
+    [...fieldset.querySelectorAll('input')]
+        .forEach((input) => input.remove());
+
+
+function clearRecipeForm() {
+    recipeForm.removeAttribute('data-id');
+    [ingredientsFieldset, instructionsFieldset].forEach(clearFieldsFromFieldset);
+    recipeForm.reset();
 }
 
-function handleAddRecipeClick(event) {
+
+function addFieldsToFieldset(fieldset, values=[null]) {
+    const newInputCount = getInputCountFromColumnCount(fieldset, values);
+    const attributes = getInputAttributesFromFieldset(fieldset);
+    const newFieldsHtml = getTextInputSetHtml(newInputCount, attributes, values) 
+
+    fieldset.insertAdjacentHTML('beforeend', newFieldsHtml)
+}
+
+
+function populateAddForm() {
+    modalHeader.textContent = 'New recipe';
+    addFieldsToFieldset(ingredientsFieldset, Array.from({length: 8}));
+    addFieldsToFieldset(instructionsFieldset, Array.from({length: 4}));
+}
+
+
+function populateEditForm(recipeId) {
+    const recipe = getRecipeById(recipeId);
+    
+    modalHeader.textContent = 'Edit recipe';
+    recipeForm.setAttribute('data-id', recipeId);
+    recipeForm.name.value = recipe.name;
+    recipeForm.description.value = recipe.description;
+
+    addFieldsToFieldset(ingredientsFieldset, recipe.ingredients);
+    addFieldsToFieldset(instructionsFieldset, recipe.instructions);
+}
+
+
+/* Update state of modal */
+
+const closeModal = () => modalOuterElement.classList.remove('open');
+
+
+const openModal = () => modalOuterElement.classList.add('open');
+
+
+
+/* ---------- GET DATA FROM THE DOM ---------- */
+
+const getInputsInFieldset = (fieldset) => [...fieldset.children]
+    .filter((element) => element.matches('input'));
+
+
+const getValuesFromFieldset = (fieldset) => getInputsInFieldset(fieldset)
+    .filter((input) => input.value)
+    .map((input) => input.value);
+
+
+const getValuesFromForm = (form) => ({
+    id: form.dataset.id || crypto.randomUUID(),
+    name: form.name.value, 
+    description: form.description.value,
+    ingredients: getValuesFromFieldset(ingredientsFieldset),
+    instructions: getValuesFromFieldset(instructionsFieldset),
+});
+
+
+
+/* ---------- GET THE STATE OF THE DOM ---------- */
+
+const roundUpToFillColumns = (initialFieldCount, columnCount) => 
+    Math.ceil(initialFieldCount / columnCount) * columnCount;
+
+
+const getGridColumnCount = (element) => getComputedStyle(element)
+    .gridTemplateColumns
+    .split(' ')
+    .length;
+
+
+function getInputCountFromColumnCount(fieldset, values) {
+    const columnCount = getGridColumnCount(fieldset);
+    const newInputCount = roundUpToFillColumns(values.length, columnCount);
+    return newInputCount
+}
+
+
+function getInputAttributesFromFieldset(fieldset) {
+    const inputs = fieldset.querySelectorAll('input');
+    const lastExtantInputName = inputs[inputs.length - 1]?.name;
+    const classList = fieldset.dataset.childClassList;
+    const nameStart = lastExtantInputName ? (Number(lastExtantInputName) + 1) : 0;
+    return {nameStart, classList}
+}
+
+
+
+/* ---------- EVENT HANDLERS ---------- */
+
+/* Third-tier event handlers */
+
+function handleSave(event) {
+    event.preventDefault();
+    return getValuesFromForm(event.currentTarget);
+}
+
+
+
+/* Second-tier event handlers */
+
+function handleAdd(event) {
+    const recipe = handleSave(event);
+    addRecipe(recipe);
+}
+
+
+function handleUpdate(event) {
+    const recipe = handleSave(event);
+    updateRecipe(recipe);
+}
+
+
+function handleEditClick(event, recipeId) {
+    toggleOptionsMenu(event);
+    populateEditForm(recipeId);
+    openModal();
+}
+
+
+function handleDeleteClick(event, recipeId) {
+    toggleOptionsMenu(event);
+    deleteRecipe(recipeId);
+}
+
+
+const handleAddInputClick = (event) => {
+    const previousSibling = event.target.previousElementSibling;
+    
+    if (previousSibling.matches('fieldset')) {
+        addFieldsToFieldset(event.target.previousElementSibling)
+    }
+};
+
+
+function handleModalClose() {
+    clearRecipeForm();
+    closeModal();
+}
+
+
+
+/* First-tier event handlers (mostly for delegation) */
+
+function handleAddRecipeClick() {
     populateAddForm();
     openModal();
 }
 
 
-addRecipeButton.addEventListener('click', handleAddRecipeClick);
-recipeForm.addEventListener('submit', (event) => {
+function handleRecipeClick(event) {
+    const recipeId = event.target.closest('.recipe-card')?.dataset.id;
+
+    if (event.target.matches('.see-options')) {
+        toggleOptionsMenu(event);
+    } else if (event.target.matches('.edit-recipe')) {
+        handleEditClick(event, recipeId)
+    } else if (event.target.matches('.delete-recipe')) {
+        handleDeleteClick(event, recipeId);
+    }
+}
+
+
+function handleModalClick(event) {
+    const isInInnerModal = event.target.closest('.modal-inner');
+
+    if (
+        !isInInnerModal 
+        || event.target.matches('button[name="cancel"]')
+    ) {
+        handleModalClose();
+
+    } else if (event.target.matches('.add-input')) {
+        handleAddInputClick(event);
+    }
+}
+
+
+const handleKeyDown = (event) => {(event.key === 'Escape') && handleModalClose()};
+
+
+function handleFormSubmission(event) {
     if (event.currentTarget.dataset.id) {
         handleUpdate(event);
     } else {
         handleAdd(event);
     }
-    closeModal();
-});
+    handleModalClose();
+}
+
+
+
+/* ---------- EVENT LISTENERS ---------- */
+
+addRecipeButton.addEventListener('click', handleAddRecipeClick);
+recipeForm.addEventListener('submit', handleFormSubmission);
 recipeListElement.addEventListener('click', handleRecipeClick);
 recipeListElement.addEventListener('recipesUpdated', displayRecipeCards);
 recipeListElement.addEventListener('recipesUpdated', mirrorRecipesToLocalStorage);
 modalOuterElement.addEventListener('click', handleModalClick);
-window.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-        closeModal();
-    }
-});
+window.addEventListener('keydown', handleKeyDown);
 
 
-function preloadData() {
-    localStorage.setItem('recipes',  JSON.stringify(
-        [{
-            id: crypto.randomUUID(),
-            name: 'Millionaire pound cake',
-            description: 'Why a millionaire pound cake? Because it\'s super rich! This scrumptious cake is the pride of an elderly belle from Jackson, Mississippi. The recipe comes from "The Glory of Southern Cooking" by James Villas.',
-            instructions: [
-                'Preheat the oven to 300 degrees',
-                'Grease a 10-inch tube pan with butter, dust the bottom and sides with flour, and set aside',
-                'In a large mixing bowl, cream the butter and sugar with an electric mixer and add the eggs one at a time, beating after each addition', 
-                'Alternately add the flour and milk, stirring till the batter is smooth',
-                'Add the two extracts and stir till well blended',
-                'Scrape the batter into the prepared pan and bake till a cake tester or knife blade inserted in the center comes out clean , about 1 1 / 2 hours', 
-                'Cool the cake in the pan on a rack for 5 minutes, then turn it out on the rack to cool completely'
-            ],
-            ingredients: ['butter', 'sugar', 'eggs', 'all-purpose flour', 'whole milk', 'pure vanilla extract', 'almond extract'],
-        },
-        {
-            id: crypto.randomUUID(),
-            name: 'Berry french toast  oatmeal',
-            description: 'the first time i made oatmeal this way i thought it tasted like french toast topped with berries...thus the name! :) use whichever kind of berries you like...my personal favorite is cherries.',
-            instructions: ['Add 1/2 cup old-fashioned oats and 1 cup water into a large microwaveable bowl', 'Cook in microwave on 50% power for 6 minutes', 'Place frozen berries in small bowl and defrost in microwave until the juice from the berries is released', 'Add defrosted berries, sugar free syrup, butter spray, and flax seed together and stir well.', 'Enjoy!'],
-            ingredients : ['old fashioned oats', 'water', 'berries', 'ground flax seeds', 'sugar-free syrup', 'i can\'t believe it\'s not butter spread'],
-            photo: 'assets/recipes/berry-oatmeal.jpg',
-        }, 
-        {
-            id: crypto.randomUUID(),
-            name: 'blepandekager danish apple pancakes',
-            description: 'this recipe was found at website: mindspring.com - christian\'s danish recipes',
-            instructions: [
-                'Beat the eggs lightly and add the milk',
-                'Combine the flour , sugar and salt',
-                'Stir the flour mixture into the egg mixture , stirring in the cup of cream as you mix',
-                'Fry the apple slices in butter in a skillet',
-                'Preheat oven to 500 degrees',
-                'Cover the bottom of an oven-proof baking dish , or heavy skillet , with apples',
-                'Pour the batter over slices and bake in a preheated 500 oven',
-                'When nearly done, remove from oven and sprinkle here and there with a mixture of sugar and cinnamon to taste', 'place dabs of butter on the pancake and return to oven until browned', 'just before serving , sprinkle with lemon juice , and cut into triangles'],
-            ingredients: ['eggs', 'milk', 'flour', 'sugar', 'salt', 'cream', 'apples', 'butter', 'cinnamon', 'lemon juice'],
-            photo: 'assets/recipes/danish-apple-pancakes.jpg',
-        }]
-    ));
-}
-    
-// preloadData();
-restoreFromLocalStorage();
+
+/* ---------- INITIALIZATION ---------- */
+
+restoreRecipesFromLocalStorage();
